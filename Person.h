6f4501ef56f3475eb3aa7224c410e86442a638a0
冀峰 <jifeng@strongunion.com.cn>
2018-05-06 18:24:15 +0800

    first commit

---
+++ Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  SwiftDemo
+//
+//  Created by JiFeng on 2018/5/6.
+//  Copyright © 2018年 JiFeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, copy) NSString *name;
+
+- (void)printPersonInfo;
+
+@end
---
+++ Person.m
@@ -0,0 +1,17 @@
+//
+//  Person.m
+//  SwiftDemo
+//
+//  Created by JiFeng on 2018/5/6.
+//  Copyright © 2018年 JiFeng. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+- (void)printPersonInfo {
+    NSLog("hello jifeng");
+}
+
+@end
